44a951843885d5aedf720f053601ab5c5bfbf404
Klaus Greiner <klausgreinerfiorini@gmail.com>
2024-05-01 18:29:36 -0300

    adding physics jumping and collisions

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # pixel_adventure
 
 Following this tutorial
-https://youtu.be/HrRzqV020wc?list=PLRRATgFqhVCh8qD7xmaSbwG1vfaCddvCM&t=2299
+https://www.youtube.com/watch?v=D1ZQFlH4g0E&list=PLRRATgFqhVCh8qD7xmaSbwG1vfaCddvCM&index=5&ab_channel=Spellthorn
+
+Player is having trouble to get on platforms
+Weird hitting head movement
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,24 +1,117 @@
-import 'dart:ui';
+import 'dart:io';
 
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pixel_adventure/components/player.dart';
 
-import 'levels/level.dart';
+import 'components/jump_button.dart';
+import 'components/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
-  @override
-  final world = Level();
 
+  Player player = Player(character: 'Pink Man');
+  late CameraComponent cam;
+  late JoystickComponent joystick;
+  bool showControls = Platform.isAndroid || Platform.isIOS;
+  int currentLevelIndex = 0;
+  bool playSounds = true;
+  double soundVolume = 1.0;
+
+  List<String> levelNames = ['Level-01', 'Level-01'];
   @override
   Future<void> onLoad() async {
     await images.loadAllImages();
 
-    camera = CameraComponent.withFixedResolution(width: 640, height: 360);
-    camera.viewfinder.anchor = Anchor.topLeft;
     addAll([camera, world]);
+    _loadLevel();
 
+    if (showControls) {
+      addJoystick();
+      add(JumpButton());
+    }
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showControls) updateJoystick();
+    super.update(dt);
+  }
+
+  void _loadLevel() {
+    Future.delayed(const Duration(seconds: 1), () {
+      Level world = Level(
+        player: player,
+        levelName: levelNames[currentLevelIndex],
+      );
+
+      cam = CameraComponent.withFixedResolution(
+        world: world,
+        width: 640,
+        height: 360,
+      );
+      cam.viewfinder.anchor = Anchor.topLeft;
+
+      addAll([cam, world]);
+    });
+  }
+
+  void loadNextLevel() {
+    removeWhere((component) => component is Level);
+
+    if (currentLevelIndex < levelNames.length - 1) {
+      currentLevelIndex++;
+      _loadLevel();
+    } else {
+      // no more levels
+      currentLevelIndex = 0;
+      _loadLevel();
+    }
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      knobRadius: 32,
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 8, bottom: 8),
+    );
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+        player.horizontalMovement = -1;
+      case JoystickDirection.upLeft:
+        player.horizontalMovement = -1;
+      case JoystickDirection.downLeft:
+        player.horizontalMovement = -1;
+        break;
+      case JoystickDirection.right:
+        player.horizontalMovement = 1;
+      case JoystickDirection.upRight:
+        player.horizontalMovement = 1;
+      case JoystickDirection.downRight:
+        player.horizontalMovement = 1;
+        break;
+      default:
+        player.horizontalMovement = 0;
+        break;
+    }
   }
 }
